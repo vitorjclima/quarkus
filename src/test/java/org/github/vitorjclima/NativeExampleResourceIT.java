186cd9a199b1f8456b6f40b61d5d3e113551382f
Vitor <vitor.lima@wps-sa.com.br>
2020-04-11 00:29:48 -0300

    Starting project

---
+++ src/test/java/org/github/vitorjclima/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.github.vitorjclima;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
